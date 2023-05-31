37fdf9630096a418aad30c561e7d5f9f635daa64
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-05-31 18:07:32 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+[Header("default qajumpisgn sapoewr")]
+public float jumpPower = 6f;
+
+[Header("bovoldea n isd goru8nde")]
+public bool isGrounded = false;
+
+float posX = 0.0f;
+
+Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
